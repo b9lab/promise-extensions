de1393c40bcbb41912c44c2b54eac61ad38765c9
Xavier Lepretre <xavier.lepretre@gmail.com>
2017-07-30 19:42:52 +0200

    Added all sequential with named objects.

--- test/allSequentialNamed.js
+++ test/allSequentialNamed.js
@@ -4,33 +4,33 @@
 const chai = require('chai');
 const expect = chai.expect;
 const Promise = require("bluebird");
-Promise.allSequential = require("../utils/allSequential.js");
+Promise.allSequentialNamed = require("../utils/allSequentialNamed.js");
 
 describe("allSequential", function() {
-    it("should resolve all promises with proper place in array", function() {
-        return Promise.allSequential([
-            () => Promise.resolve("a"),
-            () => Promise.resolve("b")
-        ])
+    it("should resolve all promises with mapped names", function() {
+        return Promise.allSequentialNamed({
+            a: () => Promise.resolve("a"),
+            b: () => Promise.resolve("b")
+        })
             .then(results => {
-                expect(results).to.deep.equal([ "a", "b" ]);
-            })
+                expect(results).to.deep.equal({ a: "a", b: "b" });
+            });
     });
 
     it("should not initiate before the previous has finished", function() {
         const trace = [];
-        return Promise.allSequential([
-            () => new Promise(resolve => {
+        return Promise.allSequentialNamed({
+            a: () => new Promise(resolve => {
                 trace.push("a");
                 resolve("b");
             })
                 .then(x => trace.push(x)),
-            () => new Promise(resolve => {
+            b: () => new Promise(resolve => {
                 trace.push("c");
                 resolve("d");
             })
                 .then(x => trace.push(x))
-        ])
+        })
             .then(results => {
                 expect(trace).to.deep.equal([ "a", "b", "c", "d" ]);
             });
@@ -39,10 +39,10 @@ describe("allSequential", function() {
     it("should keep execution order in case of delay", function() {
         this.slow(400);
         const trace = [];
-        return Promise.allSequential([
-            () => Promise.resolve("a").delay(300).then(x => trace.push(x)),
-            () => Promise.resolve("b").then(x => trace.push(x))
-        ])
+        return Promise.allSequentialNamed({
+            a: () => Promise.resolve("a").delay(300).then(x => trace.push(x)),
+            b: () => Promise.resolve("b").then(x => trace.push(x))
+        })
             .then(results => {
                 expect(trace).to.deep.equal([ "a", "b" ]);
             });
